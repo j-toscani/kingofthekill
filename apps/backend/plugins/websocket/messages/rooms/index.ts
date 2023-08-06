9ec66379a5773fca37d7171f9ad8ff590fd3722d
Julian Toscani <julian.toscani@grapes.de>
2023-08-06 22:17:36 +0200

    feat(rooms): provide add and remove room

--- apps/backend/plugins/websocket/messages/rooms/index.ts
+++ apps/backend/plugins/websocket/messages/rooms/index.ts
@@ -1,19 +1,23 @@
 import { getRooms } from './getRooms';
-import { RoomHandlerFactory, joinRoomFactory, leaveRoomFactory } from './handlers';
+import { RoomHandlerFactory, createRoomFactory, joinRoomFactory, leaveRoomFactory } from './handlers';
 
-export const joinRoom = joinRoomFactory({ getRooms });
-export const leaveRoom = leaveRoomFactory({ getRooms });
+const joinRoom = joinRoomFactory({ getRooms });
+const leaveRoom = leaveRoomFactory({ getRooms });
+const createRoom = createRoomFactory({ getRooms });
+const removeRoom = leaveRoomFactory({ getRooms });
 
 enum RoomEvents {
 	JOIN = 'join',
 	LEAVE = 'leave',
-	// CREATE = 'create',
-	// REMOVE = 'remove',
+	CREATE = 'create',
+	REMOVE = 'remove',
 }
 
 export const events: { [key in RoomEvents]: ReturnType<RoomHandlerFactory> } = {
 	join: joinRoom,
 	leave: leaveRoom,
+	create: createRoom,
+	remove: removeRoom
 } as const;
 
 export const isRoomEvent = (name: string): name is RoomEvents => RoomEvents[name.toUpperCase()];
