fa5319fc32ee1a860bea3031ab69cb46761f0b84
Julian Toscani <julian.toscani@grapes.de>
2023-08-06 22:55:16 +0200

    fix: imports

--- apps/backend/plugins/router/index.ts
+++ apps/backend/plugins/router/index.ts
@@ -1,11 +1,11 @@
 import { FastifyInstance, FastifyPluginOptions } from 'fastify';
-import { createHandleUpgrade } from '@plugins/websocket/handleUpgradeFactory.js';
-import { NotFoundError } from '@plugins/errors/ApiErrors.js';
-import { getUser } from '@plugins/websocket/getUser.js';
-import { getRooms } from '@plugins/websocket/messages/rooms/getRooms.js';
+import { handleUpgradeFactory } from '../websocket/handleUpgradeFactory.js';
+import { NotFoundError } from '../errors/ApiErrors.js';
+import { getUser } from '../websocket/getUser.js';
+import { getRooms } from '../websocket/messages/rooms/getRooms.js';
 
 export function routes(fastify: FastifyInstance, _options: FastifyPluginOptions, done: () => void) {
-	const handleUpgrade = createHandleUpgrade({ wss: fastify.wss, getUser });
+	const handleUpgrade = handleUpgradeFactory({ wss: fastify.wss, getUser });
 	fastify.server.on('upgrade', handleUpgrade);
 
 	fastify.get('/', (_request, _reply) => {
